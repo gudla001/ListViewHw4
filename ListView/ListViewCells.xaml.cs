7cd7fd851a44ab27759ce845f847f06c39fc2d1b
Pall <gudla001@cougars.csusm.edu>
2018-09-27 18:30:07 -0700

    first commit

---
+++ ListView/ListViewCells.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ListView
+{
+    public partial class ListViewCells : ContentView
+    {
+        public ListViewCells()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListView/Models /People.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+{
+    public class People
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    public string GradeLevel
+    {
+        get;
+        set;
+    }
+
+    public double Gpa
+    {
+        get;
+        set;
+    }
+
+    public string FavoriteHobby
+    {
+        get;
+        set;
+    }
+}
+}
